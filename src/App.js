f0d6c59ad99ccb90bf64a12424ca066d7e50b897
Masud <masud.cse.sub@gmail.com>
2025-08-02 03:55:45 +0600

    23
    Signed-off-by: Masud <masud.cse.sub@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Card from './components/Card'; // Importing the Card component
+
+import Data from './data.json'; // Importing data from a JSON file
+
 function App() {
+  console.log(Data); // Logging the imported data to the console
   return <div>
     
   <h1 className="headingStyle">Todo App</h1>
   <div className="cardContainer">
-    <Card />
-    <Card />
-    <Card />
+    <Card titleText={Data[0].title} descText={Data[0].desc}/>
+    <Card titleText={Data[1].title} descText={Data[1].desc}/>
+    <Card titleText={Data[2].title} descText={Data[2].desc}/>
+    <Card titleText={Data[3].title} descText={Data[3].desc}/>
 </div>
   </div>
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,4 @@
-const todotitle = "falsetrue";
-const todoDescr = "The strongest trends in the packaging industry all revolve around a circular economy. Why? At least in the European Union, these trends are driven primarily by political pressure and consumer perception regarding packaging. China (and now Thailand) are closing their doors to waste, environmental groups are lobbying to stop plastic pollution in the oceans and the EU continues to strengthen its resource self-reliance. Given the current supply chain crisis, this goal is now more important than ever.";
+
 const date = new Date();
 const dateName = date.getDate();
 const monthName = date.getMonth();
@@ -7,14 +6,28 @@ const currentYear = date.getFullYear();
 
 
 
-function Card(){
-  return (
+function Card(props){
+
+
+  // return (
+  //   <div className="card">
+  //     <h3 className="cardTitle">{props.titleText}</h3>
+  //     <p className="cardDesc">{props.descText}</p>
+  //     <p className="cardFooter">{dateName + "/" + monthName + "/" + currentYear}</p>
+  //   </div>
+  // );
+
+
+const{  titleText, descText } = props;
+   return (
     <div className="card">
-      <h3 className="cardTitle">{todotitle}</h3>
-      <p className="cardDesc">{todoDescr}</p>
+      <h3 className="cardTitle">{titleText}</h3>
+      <p className="cardDesc">{descText}</p>
       <p className="cardFooter">{dateName + "/" + monthName + "/" + currentYear}</p>
     </div>
   );
+
+
 }
 
 
