c6a9f737d6ee501ea6f63d621f0f642a82b143b6
Masud <masud.cse.sub@gmail.com>
2025-08-02 03:55:51 +0600

    24
    Signed-off-by: Masud <masud.cse.sub@gmail.com>

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ import Card from './components/Card'; // Importing the Card component
 import Data from './data.json'; // Importing data from a JSON file
 
 function App() {
-  console.log(Data); // Logging the imported data to the console
+  let items = [];
+  for(let x=0; x<Data.length; x++){
+    items.push(<Card titleText={Data[x].title} descText={Data[x].desc} />) // Creating Card components dynamically
+  } // Getting the number of items in the data
   return <div>
     
   <h1 className="headingStyle">Todo App</h1>
-  <div className="cardContainer">
-    <Card titleText={Data[0].title} descText={Data[0].desc}/>
-    <Card titleText={Data[1].title} descText={Data[1].desc}/>
-    <Card titleText={Data[2].title} descText={Data[2].desc}/>
-    <Card titleText={Data[3].title} descText={Data[3].desc}/>
-</div>
+  {items} {/* Rendering the Card components */}
   </div>
 }
 
